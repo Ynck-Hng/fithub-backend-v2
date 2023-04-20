bd0732347b7e915f44e5d4005a2853d8421b6724
Ynck-Hng <ynckhuang@gmail.com>
2023-04-20 12:38:14 +0200

    removed /

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 app.use(cors({
-    origin: "https://fithub-fithub2.vercel.app/",
+    origin: "https://fithub-fithub2.vercel.app",
     credentials: true,
 }));
 
